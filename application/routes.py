7578b49ef32f38e6b8fd78b541c1d8f9f3c6cdd0
Bougatsaki <duanxiaoxiao9@gmail.com>
2022-11-30 16:55:42 -0800

    mm

--- application/routes.py
+++ application/routes.py
@@ -1,37 +1,59 @@
-from application import app
+from application import app,db
 
-from flask import Flask, jsonify,render_template, flash, request, redirect, url_for
-from pyspark.sql import SparkSession, functions, types
+from flask import Flask, jsonify,render_template, flash, request, redirect, url_for, json
+#from pyspark.sql import SparkSession, functions, types
 
-spark = SparkSession.builder.appName('jsfile').getOrCreate()
-spark.sparkContext.setLogLevel('WARN')
-sc = spark.sparkContext
-jsfile = 'ab.json'
-df = spark.read.json(jsfile).cache()
+#spark = SparkSession.builder.appName('jsfile').getOrCreate()
+#spark.sparkContext.setLogLevel('WARN')
+#sc = spark.sparkContext
+#jsfile = 'ab.json'
+#df = spark.read.json(jsfile).cache()
 
-def explode(nested):
-    list=[]
-    for i in range(9):
-        list.append(nested[i][0])
-    return list
+
+
+
+
+    
+# def expand(nested):
+#     list=[]
+#     for i in range(9):
+#         list.append(nested[i][0])
+#     return list
 
 @app.route('/', methods=['GET'])
 def bar_chart():
-    result =df.groupBy('stars').count().sort("stars").cache()
+    # result =df.groupBy('stars').count().sort("stars").cache()
     
-    labels=result.select("stars").collect()
-    labels=explode(labels)#flatMap(lambda x:x[0]).collect()
+    # labels=result.select("stars").collect()
+    # labels=expand(labels)#flatMap(lambda x:x[0]).collect()
 
 
-    values=result.select("count").collect()
-    values=explode(values)
-    print(labels)
-    print(values)
+    # values=result.select("count").collect()
+    #values=expand(values)
+    return render_template("chart.html")#, labels = labels, values = values)
 
 
 
 
-    return render_template("chart.html", labels = labels, values = values)
+@app.route('/map', methods=['GET'])
+def map():
+
+    business = db.ab.find()
+   
+    business_dic = {}
+    business_list = []
+    for data in business:
+        business_dic['name'] = data['name']
+        business_dic['latitude'] = data['latitude']
+        business_dic['longitude'] = data['longitude']
+        business_list.append(business_dic)
+        business_dic = {}
+    data = json.dumps(business_list)
+    return render_template("map.html",data=data)
+
+
+
+    
 
 
 
