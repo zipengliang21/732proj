ee696ca209ca831e663b9c29b4f87ec2b0ae9173
Bougatsaki <duanxiaoxiao9@gmail.com>
2022-11-30 16:52:50 -0800

    m

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] ='657fe4b48c16dab9ba6804334bbcf1d9e77f4d75'
 
+client = MongoClient('')
+db=client.get_database('proj_db')
 
-#db=client.get_database('proj_db')
+
+
 
 
 from application import routes
