00526f174cebbff2958fb0c9ab18715704cf47fa
Bougatsaki <duanxiaoxiao9@gmail.com>
2022-12-01 20:59:43 -0800

    first

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] ='657fe4b48c16dab9ba6804334bbcf1d9e77f4d75'
+app.config["SECRET_KEY"] =''
 
 client = MongoClient('')
 db=client.get_database('proj_db')
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app,db
 
 from flask import Flask, jsonify,render_template, flash, request, redirect, url_for, json
+from bson import ObjectId
 #from pyspark.sql import SparkSession, functions, types
 
 #spark = SparkSession.builder.appName('jsfile').getOrCreate()
@@ -37,19 +38,52 @@ def bar_chart():
 
 @app.route('/map', methods=['GET'])
 def map():
-
     business = db.ab.find()
-   
     business_dic = {}
     business_list = []
     for data in business:
         business_dic['name'] = data['name']
         business_dic['latitude'] = data['latitude']
+        business_dic['stars'] = data['stars']
+        business_dic['review_count'] = data['review_count'] 
         business_dic['longitude'] = data['longitude']
         business_list.append(business_dic)
         business_dic = {}
-    data = json.dumps(business_list)
+        data = json.dumps(business_list)
     return render_template("map.html",data=data)
+
+
+@app.route('/map/<s>', methods=['GET'])
+def star_map(s):
+        business = db.ab.find({ 'stars': int(s) })
+        business_dic = {}
+        business_list = []
+        for data in business:
+            business_dic['name'] = data['name']
+            business_dic['latitude'] = data['latitude']
+            business_dic['stars'] = data['stars']
+            business_dic['review_count'] = data['review_count'] 
+            business_dic['longitude'] = data['longitude']
+            business_list.append(business_dic)
+            business_dic = {}
+        data = json.dumps(business_list)
+        return render_template("map.html",data=data)
+
+@app.route('/location', methods=['GET'])
+def location():
+        out = db.out.find()
+        out_dic = {}
+        out_list = []
+        for data in out:
+            out_dic['name'] = data['store']
+            out_dic['longitude'] = data['initial longitude']
+            out_dic['latitude'] = data['initial latitude']
+            out_list.append(out_dic)
+            out_dic = {}
+        data = json.dumps(out_list)
+        
+        return render_template("location.html",data=data)
+
 
 
 
