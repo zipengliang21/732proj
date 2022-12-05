55c1dfdc39fcc850ba37da8447261c48c1572227
Zipeng Liang <zipengliang21@gmail.com>
2022-12-05 00:33:28 -0800

    Update API

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,5 @@
 from application import app,db
+from flask_cors import CORS
 
 from flask import Flask, jsonify,render_template, flash, request, redirect, url_for, json
 from bson import ObjectId
@@ -11,7 +12,7 @@ from bson import ObjectId
 #df = spark.read.json(jsfile).cache()
 
 
-
+CORS(app)
 
 
     
@@ -38,7 +39,7 @@ def bar_chart():
 
 @app.route('/map', methods=['GET'])
 def map():
-    business = db.ab.find()
+    business = db.ab_full_c.find()
     business_dic = {}
     business_list = []
     for data in business:
@@ -49,13 +50,13 @@ def map():
         business_dic['longitude'] = data['longitude']
         business_list.append(business_dic)
         business_dic = {}
-        data = json.dumps(business_list)
-    return render_template("map.html",data=data)
+    data = json.dumps(business_list)
+    return data
 
 
-@app.route('/map/<s>', methods=['GET'])
+@app.route('/map/s/<s>', methods=['GET'])
 def star_map(s):
-        business = db.ab.find({ 'stars': int(s) })
+        business = db.ab_full_c.find({ 'stars': float(s) })
         business_dic = {}
         business_list = []
         for data in business:
@@ -67,7 +68,97 @@ def star_map(s):
             business_list.append(business_dic)
             business_dic = {}
         data = json.dumps(business_list)
-        return render_template("map.html",data=data)
+        return data
+
+@app.route('/map/c/<c>', methods=['GET'])
+def category_map(c):
+        business = db.ab_full_c.find({ 'category': c })
+        business_dic = {}
+        business_list = []
+        for data in business:
+            business_dic['name'] = data['name']
+            business_dic['latitude'] = data['latitude']
+            business_dic['stars'] = data['stars']
+            business_dic['review_count'] = data['review_count'] 
+            business_dic['longitude'] = data['longitude']
+            business_list.append(business_dic)
+            business_dic = {}
+        data = json.dumps(business_list)
+        return data
+
+@app.route('/map/sc/<s>/<c>', methods=['GET'])
+def star_category_map(s, c):
+        business = db.ab_full_c.find({ 'stars': float(s), 'category': c })
+        business_dic = {}
+        business_list = []
+        for data in business:
+            business_dic['name'] = data['name']
+            business_dic['latitude'] = data['latitude']
+            business_dic['stars'] = data['stars']
+            business_dic['review_count'] = data['review_count'] 
+            business_dic['longitude'] = data['longitude']
+            business_list.append(business_dic)
+            business_dic = {}
+        data = json.dumps(business_list)
+        return data
+
+@app.route('/starCount', methods=['GET'])
+def star_count():
+        star_1 = db.ab_full_c.count_documents({ 'stars': float(1) })
+        star_1_5 = db.ab_full_c.count_documents({ 'stars': float(1.5) })
+        star_2 = db.ab_full_c.count_documents({ 'stars': float(2) })
+        star_2_5 = db.ab_full_c.count_documents({ 'stars': float(2.5) })
+        star_3 = db.ab_full_c.count_documents({ 'stars': float(3) })
+        star_3_5 = db.ab_full_c.count_documents({ 'stars': float(3.5) })
+        star_4 = db.ab_full_c.count_documents({ 'stars': float(4) })
+        star_4_5 = db.ab_full_c.count_documents({ 'stars': float(4.5) })
+        star_5 = db.ab_full_c.count_documents({ 'stars': float(5) })
+
+        star_count = []
+        star_count.append(star_1)
+        star_count.append(star_1_5)
+        star_count.append(star_2)
+        star_count.append(star_2_5)
+        star_count.append(star_3)
+        star_count.append(star_3_5)
+        star_count.append(star_4)
+        star_count.append(star_4_5)
+        star_count.append(star_5)
+
+        data = json.dumps(star_count)
+
+        return data
+
+@app.route('/categoryCount', methods=['GET'])
+def category_count():
+        restaurants = db.ab_full_c.count_documents({ 'category': "Restaurants" })
+        food = db.ab_full_c.count_documents({ 'category': "Food" })
+        nightlife = db.ab_full_c.count_documents({ 'category': "Nightlife" })
+        shopping = db.ab_full_c.count_documents({ 'category': "Shopping" })
+        b_s = db.ab_full_c.count_documents({ 'category': "Beauty & Spas" })
+        local_services = db.ab_full_c.count_documents({ 'category': "Local Services" })
+        fashion = db.ab_full_c.count_documents({ 'category': "Fashion" })
+        active_life = db.ab_full_c.count_documents({ 'category': "Active Life" })
+        automotive = db.ab_full_c.count_documents({ 'category': "Automotive" })
+        h_m = db.ab_full_c.count_documents({ 'category': "Health & Medical" })
+        others = db.ab_full_c.count_documents({ 'category': "Others" })
+
+        category_count = []
+        category_count.append(restaurants)
+        category_count.append(food)
+        category_count.append(nightlife)
+        category_count.append(shopping)
+        category_count.append(b_s)
+        category_count.append(local_services)
+        category_count.append(fashion)
+        category_count.append(active_life)
+        category_count.append(automotive)
+        category_count.append(h_m)
+        category_count.append(others)
+
+        data = json.dumps(category_count)
+
+        return data
 
 @app.route('/location', methods=['GET'])
 def location():
@@ -78,11 +169,14 @@ def location():
             out_dic['name'] = data['store']
             out_dic['longitude'] = data['initial longitude']
             out_dic['latitude'] = data['initial latitude']
+            out_dic['neighbor_store'] = data['neighbor store']
+            out_dic['neighbor_longitude'] = data['neighbor longitude']
+            out_dic['neighbor_latitude'] = data['neighbor latitude']
             out_list.append(out_dic)
             out_dic = {}
         data = json.dumps(out_list)
         
-        return render_template("location.html",data=data)
+        return data
 
 
 
